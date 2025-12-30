fcdf5b06113ab3e2d9e2affcacc52d6ff5fdf4ba
krishsingh10386@gmail.com <bt23eci047@iiit.ac.in>
2025-12-30 23:58:13 +0530

    feat: Added singleton js design pattern in DB connection

--- src/utils/markdownSanitizer.js
+++ src/utils/markdownSanitizer.js
@@ -1,44 +1,30 @@
 const sanitizeHtmlLibrary = require("sanitize-html");
-const markdown = require("marked");
-const TurndownService = require('turndown');
+// const markdown = require("marked");
+const TurndownService = require("turndown");
 
+async function sanitizeMarkdownContent(markdownContent) {
+  const { marked } = await import("marked"); // ✅ ESM-safe
 
+  const turndownService = new TurndownService();
 
+  // 1. convert markdown to html
+  const convertedHtml = markdown.parse(markdownContent);
+  // console.log("markdown data", convertedHtml);
 
-function sanitizeMarkdownContent(markdownContent) {
-    const turndownService = new TurndownService();
+  // 2. sanitized html
+  const sanitizedHtml = sanitizeHtmlLibrary(convertedHtml, {
+    allowedTags: sanitizeHtmlLibrary.defaults.allowedTags.concat(["img"]),
+  });
+  // console.log("sanitized data", sanitizedHtml);
 
+  // 3. Convert the sanitized html back to markdown
+  const sanitizedMarkdown = turndownService.turndown(sanitizedHtml);
+  // console.log("Sanitized markdown", sanitizedMarkdown);
 
-    // 1. convert markdown to html
-    const convertedHtml = markdown.parse(markdownContent);
-    // console.log("markdown data", convertedHtml);
-
-
-
-    // 2. sanitized html
-    const sanitizedHtml = sanitizeHtmlLibrary(convertedHtml, {
-        allowedTags: sanitizeHtmlLibrary.defaults.allowedTags.concat(['img'])
-    });
-    // console.log("sanitized data", sanitizedHtml);
-
-
-
-    // 3. Convert the sanitized html back to markdown
-    const sanitizedMarkdown = turndownService.turndown(sanitizedHtml);
-    // console.log("Sanitized markdown", sanitizedMarkdown);
-
-
-
-    return sanitizedMarkdown;
+  return sanitizedMarkdown;
 }
 
-
 module.exports = sanitizeMarkdownContent;
-
-
-
-
-
 
 // const input = `
 // # Hello
